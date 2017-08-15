a5afca0608f7acdb62d861a8c4f1667c5f5b182e
Nazar <cutlass90@mail.ru>
2017-08-15 16:48:06 +0300

    add get_cifar10_loaders function

--- ladder_tools.py
+++ ladder_tools.py
@@ -2,9 +2,12 @@ from tensorflow.contrib.learn.python.learn.datasets import base
 from tensorflow.contrib.learn.python.learn.datasets.mnist import extract_images
 from tensorflow.contrib.learn.python.learn.datasets.mnist import extract_labels
 from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
+from tensorflow.python.framework import dtypes
+
 import numpy as np
 
 import tools
+import cifar10
 
 
 def read_data_sets(train_dir):
@@ -39,12 +42,7 @@ def read_data_sets(train_dir):
     return train_images, train_labels, test_images, test_labels
 
 
-class ImageProvider:
-    def __init__(self, images, labels):
-        self.loader = DataSet(images, labels, one_hot=True)
-    def next_batch(self, batch_size):
-        return self.loader.next_batch(batch_size)[0]
-
+# ------------------------------------------------------------------------------
 def get_mnist_loaders(class_distrib_lab, class_distrib_unlab):
     train_im, train_l, test_im, test_l = read_data_sets("datasets/MNIST")
     lab_im, lab_t, rest_im, rest_t = tools.split_data_set(class_distrib_lab,
@@ -60,7 +58,40 @@ def get_mnist_loaders(class_distrib_lab, class_distrib_unlab):
     labeled_data_loader = DataSet(lab_im, lab_t, one_hot=True)
     test_data_loader = DataSet(test_im, test_l, one_hot=True)
 
-    image_provider = ImageProvider(np.vstack((lab_im, unlab_im)),
-                                    np.vstack((lab_t, unlab_t)))
+    loader = DataSet(np.vstack((lab_im, unlab_im)), np.vstack((lab_t, unlab_t)),
+        one_hot=True)
+    image_provider = lambda bs: loader.next_batch(bs)[0]
+    
 
     return image_provider, labeled_data_loader, test_data_loader
+
+
+# ------------------------------------------------------------------------------
+def get_cifar10_loaders(class_distrib_lab):
+    cifar10.maybe_download_and_extract()
+    names = cifar10.load_class_names()
+    print('There are class names in the dataset:')
+    [print(n) for n in names]
+    train_im, _, train_l= cifar10.load_training_data()
+    test_im, _, test_l= cifar10.load_test_data()
+
+    lab_im, lab_t, unlab_im, unlab_t = tools.split_data_set(class_distrib_lab,
+        train_im, train_l, verbose=True)
+
+    print('\n labeled summary:')
+    tools.print_stat(lab_t)
+    print('\n unlabeled summary:')
+    tools.print_stat(unlab_t)
+
+    labeled_data_loader = DataSet(lab_im, lab_t, dtype=dtypes.uint8, reshape=False)
+    test_data_loader = DataSet(test_im, test_l, dtype=dtypes.uint8, reshape=False)
+
+    loader = DataSet(np.vstack((lab_im, unlab_im)), np.vstack((lab_t, unlab_t)),
+        dtype=dtypes.uint8, reshape=False)
+    image_provider = lambda bs: loader.next_batch(bs)[0]
+
+    return image_provider, labeled_data_loader, test_data_loader
+
+
+
+
